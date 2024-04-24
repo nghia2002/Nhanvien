6e5f907421fda02a5ca1debb27e6af2adb1e4e6b
John Doe <onlykar98@gmail.com>
2024-04-24 10:15:38 +0700

    first commit

---
+++ src/NhanVien.java
@@ -0,0 +1,20 @@
+
+public class NhanVien {
+    protected String ten;
+    protected long luong;
+
+    public NhanVien() {
+
+    }
+    public NhanVien(String ten) {
+        this.ten = ten;
+    }
+    protected String loaiNhanVien() {
+        return "";
+    }
+    public void xuatThongTin() {
+        System.out.print("Nhan vien : " + ten);
+        System.out.print("Loai nhan vien : " + loaiNhanVien());
+        System.out.print("Luong : " + luong);
+    }
+}
